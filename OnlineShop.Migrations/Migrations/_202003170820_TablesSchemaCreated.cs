e0599ffff99b395c5e1b08ef6b076c3b649157af
Aref Dehghanian <aref.dehghanian@gmail.com>
2021-03-17 11:40:06 +0330

    Migrations layer added.

---
+++ OnlineShop.Migrations/Migrations/_202003170820_TablesSchemaCreated.cs
@@ -0,0 +1,70 @@
+using FluentMigrator;
+
+namespace OnlineShop.Migrations
+{
+    [Migration(202003170820)]
+    public class _202003170820_TablesSchemaCreated : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("ProductCategories")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Title").AsString(50).NotNullable().Unique();
+
+            Create.Table("Products")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Title").AsString(50).NotNullable()
+                .WithColumn("ProductCode").AsString(10).NotNullable()
+                .WithColumn("MinimumStock").AsInt32().NotNullable()
+                .WithColumn("ProductCategoryId").AsInt32().NotNullable()
+                .ForeignKey("ProductCategories", "Id");
+
+            Create.Table("WarehouseItems")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Stock").AsInt32().NotNullable()
+                .WithColumn("ProductId").AsInt32().NotNullable()
+                .ForeignKey("Products", "Id");
+
+            Create.Table("Purchases")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("InvoiceNumber").AsString(20).NotNullable()
+                .WithColumn("PurchaseDate").AsDateTime().NotNullable()
+                .WithColumn("Count").AsInt32().NotNullable()
+                .WithColumn("ProductId").AsInt32().NotNullable()
+                .ForeignKey("Products", "Id");
+
+            Create.Table("Invoices")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("CustomerName").AsString(50).NotNullable()
+                .WithColumn("InvoiceNumber").AsString(20).NotNullable();
+
+            Create.Table("InvoiceItems")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Price").AsDecimal().NotNullable()
+                .WithColumn("Count").AsInt32().NotNullable()
+                .WithColumn("ProductId").AsInt32().NotNullable()
+                .ForeignKey("Products", "Id")
+                .WithColumn("InvoiceId").AsInt32().NotNullable()
+                .ForeignKey("Invoices", "Id");
+
+            Create.Table("AccountingDocuments")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("DocumentNumber").AsString(20).NotNullable()
+                .WithColumn("TotalPrice").AsDecimal().NotNullable()
+                .WithColumn("DocumentRegistrationDate").AsDateTime().NotNullable()
+                .WithColumn("InvoiceId").AsInt32().NotNullable()
+                .ForeignKey("Invoices", "Id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("AccountingDocuments");
+            Delete.Table("InvoiceItems");
+            Delete.Table("Invoices");
+            Delete.Table("Purchases");
+            Delete.Table("WarehouseItems");
+            Delete.Table("Products");
+            Delete.Table("ProductCategories");
+        }
+    }
+}
---
+++ OnlineShop.Migrations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OnlineShop.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer()
+                    .WithGlobalConnectionString("Server=.; Initial Catalog=OnlineShop; Integrated Security=true;")
+                    .ScanIn(typeof(Program).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            runner.MigrateUp();
+        }
+    }
+}
